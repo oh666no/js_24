148d20449ae685482ea7ccb6ae1632a4b4e0bcd8
Paul Kotula <pavlokotula@gmail.com>
2018-04-18 20:06:17 +0300

    init

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+
+    document.getElementById('submit').onclick = function(){
+        var numbers = document.getElementsByClassName('number');
+        var sum = 0;
+
+        for (var i = 0; i < numbers.length; i++) {
+            sum += +numbers[i].value;
+        }
+
+        document.getElementById('result').innerHTML = "сума двох чисел: "+sum;
+    };
